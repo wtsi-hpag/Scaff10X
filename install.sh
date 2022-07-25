3e005cd5fefd27642ab8f72d503f15aa81c7311b
Dinindu Senanayake <dinindusen@gmail.com>
2022-07-25 15:50:00 +1200

    fix pigz-2.6 url

--- install.sh
+++ install.sh
@@ -80,7 +80,7 @@ if [[ ! -s $bindir/pigz ]]; then
 
     if [[ ! -d $projdir/src/pigz ]]; then
 	cd $projdir/src/
-        wget -r -np -nd https://zlib.net/pigz/pigz-2.6.tar.gz &> $projdir/src/log/pigz_wget.log
+        wget -r -np -nd https://zlib.net/pigz/fossils/pigz-2.6.tar.gz &> $projdir/src/log/pigz_wget.log
         tar -xvzf pigz-2.6.tar.gz &> $projdir/src/log/pigz_untar.log
         rm -f pigz-2.6.tar.gz
     fi
